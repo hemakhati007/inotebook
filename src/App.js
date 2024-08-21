c7d687e4acafcc861410f4b9f896821fcf5fddd1
hema <hemakhati43@gmail.com>
2024-08-22 05:05:52 +0530

    log

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
+import Log from "./components/Log";
+
 
 import About from "./components/About";
-;
+
 
 import NoteState from "./contexts/notes/NotesState";
 import Alert from "./components/Alert";
@@ -20,9 +22,8 @@ function App() {
           <div className="container">
             <Routes>
               <Route exact path="/" element={<Home />} />
-           
-
-
+              <Route exact path="/Login" element={<log />} />
+             
               <Route exact path="/about" element={<About />} />
               
             </Routes>
