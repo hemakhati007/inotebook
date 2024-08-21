fba7f2c9a773286e6523dc01f5f3d87416e53d31
hema <hemakhati43@gmail.com>
2024-08-22 04:50:13 +0530

    just

--- src/App.js
+++ src/App.js
@@ -2,7 +2,10 @@ import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
+
 import About from "./components/About";
+;
+
 import NoteState from "./contexts/notes/NotesState";
 import Alert from "./components/Alert";
 
@@ -17,8 +20,11 @@ function App() {
           <div className="container">
             <Routes>
               <Route exact path="/" element={<Home />} />
+           
+
 
               <Route exact path="/about" element={<About />} />
+              
             </Routes>
           </div>
         </Router>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -30,26 +30,25 @@ const Navbar = () => {
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
-             <Link className={'nav-link ${location.pathname==="/"?"active":""}'} aria-current="page"to="/">
+             <Link className={`nav-link ${location.pathname==="/"?"active":""}`} aria-current="page"to="/">
                 Home
              </Link>
             </li>
             <li className="nav-item">
-             <Link className={'nav-link ${location.pathname==="/about"?"active":""}'}to="/about">
+             <Link className={`nav-link ${location.pathname==="/about"?"active":""}`}to="/about">
                 About
              </Link>
             </li>
           </ul>
           <form className="d-flex" role="search">
-            <input
+            {/* <input
               className="form-control me-2"
               type="search"
               placeholder="Search"
               aria-label="Search"
-            />
-            <button className="btn btn-outline-success" type="submit">
-              Search
-            </button>
+            /> */}
+            <Link className="btn btn-primary mx-2" to="/Login" role="button">Login</Link>
+            <Link className="btn btn-primary mx-2" to="/Signup" role="button">Sign up</Link>
           </form>
         </div>
       </div>
