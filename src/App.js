f5baa47a0607bf4bee68d83f83ea1ad043e42188
hema <hemakhati43@gmail.com>
2024-08-22 20:12:01 +0530

    to rename
    restart the environment and developmnet server see chages and  path in git

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
-import Log from "./components/Log";
 
+import Login from "./components/Login";
 
 import About from "./components/About";
+import Signup from "./components/Signup";
 
 
 import NoteState from "./contexts/notes/NotesState";
@@ -22,9 +23,9 @@ function App() {
           <div className="container">
             <Routes>
               <Route exact path="/" element={<Home />} />
-              <Route exact path="/Login" element={<log />} />
-             
+              <Route exact path="/Login" element={<Login />} />
               <Route exact path="/about" element={<About />} />
+              <Route exact path="/Signup" element={<Signup />} />
               
             </Routes>
           </div>
